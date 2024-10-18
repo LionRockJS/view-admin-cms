23d13555eb5a77f6e13b24e363351daa95f9da39
Colin Leung <colin@cowise.co>
2024-10-19 04:46:52 +0800

    fix: index

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-import url from "node:url";
-const dirname = url.fileURLToPath(new URL('.', import.meta.url)).replace(/\/$/, '');
-export default {dirname}
+export default {
+  filename : import.meta.url
+}
